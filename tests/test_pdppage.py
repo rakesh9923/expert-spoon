591dc9629afa08289a5391acbc9bd7db56e9c7fc
rakesh.das <rakesh.das9923@gmail.com>
2023-03-27 16:05:17 +0530

    first_commit

---
+++ tests/test_pdppage.py
@@ -0,0 +1,32 @@
+
+class TestPDPPage:
+
+    def test_tc11_verify_pdp_page(self):
+        assert 1 == 1
+
+    def test_tc12_verify_pdp_banner(self):
+        assert 2 == 2
+
+    def test_tc13_verify_pdp_tag(self):
+        assert 2 == 2
+
+    def test_tc14_verify_pdp_image(self):
+        assert 2 == 2
+
+    def test_tc15_verify_pdp_footer(self):
+        assert 2 == 2
+
+    def test_tc16_verify_pdp_header(self):
+        assert 2 == 2
+
+    def test_tc17_verify_pdp_latest(self):
+        assert 2 == 2
+
+    def test_tc18_verify_pdp_product(self):
+        assert 2 == 2
+
+    def test_tc19_verify_pdp_category(self):
+        assert 2 == 2
+
+    def test_tc20_verify_pdp_main(self):
+        assert 2 == 1
